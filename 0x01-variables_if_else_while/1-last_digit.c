eadc9b5d5484913cd21acf9672c544c6cde789f5
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-11 22:32:35 +0300

    Print and get 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,12 +7,22 @@
 int main(void)
 {
 	int n;
-
+        char* comparison_text;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	int reminder = n % 10;
-	char comparison_text = "hello";
-	
+	if(reminder > 5)
+	{
+	    comparison_text = "greater than 5";
+	}
+	else if(reminder == 0)
+	{
+	    comparison_text = "is 0";
+	}
+	else if(reminder < 6 && reminder > 0)
+	{
+	    comparison_text = "less than 6 and not 0";
+	}
 	printf("Last digit of %d is %d and %s", n, reminder, comparison_text);
 	return (0);
 }
