dd156583bf259588c34b3dce6bfdeeb2fd130832
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-11 14:43:31 -0700

    print capital and small

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to alphabet  small and capital program
+ * Description: it  prints the alphabet in lowercase then
+ * in uppercase , followed by a new line.
+ * Return: Always 0
+ */
+int main(void)
+{
+	char firstSmallChar = 'a';
+	char firstCapChar = 'A';
+	int c = 0;
+	int s = 0;
+
+	for (s = 0; s < 26; s++)
+	{
+		 putchar(firstSmallChar + s);
+	}
+	for (c = 0; c < 26; c++)
+	{
+		putchar(firstCapChar + c);
+	}
+	putchar('\n');
+	return (0);
+}
