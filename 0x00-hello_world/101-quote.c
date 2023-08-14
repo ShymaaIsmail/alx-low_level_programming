6bc69221eba51d917204fd5b9fba65c590428b4f
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-14 11:38:59 -0700

    print error message

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: print error message to standard output
+ *  without frptint anf fput
+ * Return: Always 1 on error
+ */
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
