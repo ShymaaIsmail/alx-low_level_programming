25fe1923ce4b7c2243e084e94872e50b281b34b1
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-14 14:25:55 -0700

    101 print comb 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
 
-int main() {
-    int i;
-    for (i = 1; i < 1000; i++) {
-        
-        int firstDigit = i / 100;
-        int secondDigit = (i / 10) % 10;
-        int thirdDigit = i % 10;
- 
-        if ((firstDigit != secondDigit && firstDigit != thirdDigit && secondDigit!= thirdDigit) && ((firstDigit < secondDigit) && (secondDigit < thirdDigit)) ) {
-            putchar(firstDigit + '0');
-            putchar(secondDigit + '0');
-            putchar(thirdDigit + '0');
-            
-            if (firstDigit + secondDigit + thirdDigit< 24) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    
-    putchar('\n');
-    
-    return 0;
+/**
+ * main - Entry point
+ * Description:  prints all possible different combinations of three digits.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 1000; i++)
+	{
+		int firstDigit = i / 100;
+		int secondDigit = (i / 10) % 10;
+		int thirdDigit = i % 10;
+
+		if ((firstDigit != secondDigit && firstDigit != thirdDigit
+					&& secondDigit != thirdDigit)
+			&& ((firstDigit < secondDigit) && (secondDigit < thirdDigit)))
+		{
+			putchar(firstDigit + '0');
+			putchar(secondDigit + '0');
+			putchar(thirdDigit + '0');
+
+			if (firstDigit + secondDigit + thirdDigit < 24)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
