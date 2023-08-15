4e1e223a490b1e236872f6a794444db3a62f5cd2
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-15 12:06:17 -0700

    print sign fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,18 +1,24 @@
+#include "main.h"
+/**
+ *print_sign - print sign based on n value
+ *@n : value to check
+ *Return : 1 if n positive, 0 if n 0 , -1 if n negative
+ */
 int print_sign(int n)
 {
-	if(n > 0)
+	if (n > 0)
 	{
-		_putchar(43+'');
+		_putchar(43 + '');
 		return (1);
 	}
-	else if(n ==0)
+	else if (n == 0)
 	{
-		_putchar(48+'');
+		_putchar(48 + '');
 		return (0);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
-		_putchr(45+'');
+		_putchr(45 + '');
 		return (-1);
 	}
 }
