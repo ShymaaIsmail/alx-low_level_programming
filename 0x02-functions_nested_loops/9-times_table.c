326874f1fe1f3d33bdc53771ecb15f0922efe45c
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-15 16:48:21 -0700

    enhance times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,13 +13,13 @@ void multiply_number(int n)
 	{
 		_putchar(',');
 		_putchar(' ');
-		result = i * j;
-		if (prod <= 9)
+		result = n * j;
+		if (result <= 9)
 			_putchar(' ');
 		else
-			_putchar((prod / 10) + '0');
+			_putchar((result / 10) + '0');
 
-		_putchar((prod % 10) + '0');
+		_putchar((result % 10) + '0');
 	}
 }
 
