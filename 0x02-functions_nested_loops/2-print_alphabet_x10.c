e253b551052685401ec3e7d9b4161d9a1b1ecf4a
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-15 08:49:47 -0700

    print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets 10 times
+ * Description: prints using print_alphabet
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for(i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+
+}
