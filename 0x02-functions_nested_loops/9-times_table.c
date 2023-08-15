ba623e3351d30c41091ab2448a534cda1b5fc40c
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-15 16:46:39 -0700

    enhance times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,32 +1,41 @@
 #include "main.h"
 /**
+ *multiply_number - multiply a number by 1 to 9
+ * @n: number
+ */
+
+void multiply_number(int n)
+{
+	int j;
+	int result;
+
+	for (j = 1; j <= 9; j++)
+	{
+		_putchar(',');
+		_putchar(' ');
+		result = i * j;
+		if (prod <= 9)
+			_putchar(' ');
+		else
+			_putchar((prod / 10) + '0');
+
+		_putchar((prod % 10) + '0');
+	}
+}
+
+/**
  *times_table - prints time table
  *
  */
+
 void times_table(void)
 {
 	int i;
-	int mult;
-	int prod;
 
 	for (i = 0; i <= 9; i++)
 	{
 		_putchar('0');
-
-		for (mult = 1; mult <= 9; mult++)
-		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = i * mult;
-
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-
-			_putchar((prod % 10) + '0');
-		}
+		multiply_number(i);
 		_putchar('\n');
 	}
 
