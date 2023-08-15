a8b80f8db5cb631eb71786f86e53bb391d3d1080
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-15 05:15:22 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- putchar printing program
+ * Description: prints _putchar
+ * Return: retuns zero
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
