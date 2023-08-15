d6c772398798de77ad33a1f269282d6459ac0a2c
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-15 09:01:03 -0700

    print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,17 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char firstChar = 'a';
+	int c = 0;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		print_alphabet();
+
+	for (c = 0; c < 26; c++)
+	{
+		_putchar(firstChar + c);
+	}
+	_putchar('\n');
 	}
 
 }
