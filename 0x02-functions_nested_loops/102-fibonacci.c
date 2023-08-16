a9e17c1febdca530c75aa5747f1a3dc00905153c
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-16 14:26:04 -0700

    fiboncai series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - fibonaci series
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 50; i++)
+	{
+		int n1 = i - 1;
+		int n2 = i - 2;
+
+		if (n1 > 0 && n2 > 0)
+		{
+			printf("%d", n1 + n2);
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i < 49)
+		{
+			putchar(' ');
+			putchar(',');
+		}
+	}
+	return (0);
+}
