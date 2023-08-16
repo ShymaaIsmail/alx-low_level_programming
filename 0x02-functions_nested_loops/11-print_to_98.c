bb5a5c4ae19b2fc10d2250c6845b1479db1d8044
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-15 17:17:01 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ *print_to_98 - print numbers from n  untill 98
+ *@n: number
+ *Return: void
+ */
+void print_to_98(int n)
+{
+	if (n == 98)
+	{
+		_putchar(n);
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			_putchar(n);
+
+			if (n != 98)
+			{
+				_putchar(' ');
+				_putchar(',');
+			}
+			n--;
+		}
+	}
+	else if (n < 98)
+	{
+		while (n <= 98)
+		{
+			_putchar(n);
+
+			if (n != 98)
+			{
+			_putchar(' ');
+			_putchar(',');
+			}
+			n++;
+		}
+	}
+}
