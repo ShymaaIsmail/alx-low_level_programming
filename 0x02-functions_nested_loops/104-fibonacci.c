d0af0a0a3a6e0aaa07b72126ccf7c82ff0eb2d9d
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-16 17:58:11 -0700

    104 fibnacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - fibonaci series
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long  lastPrinted;
+	unsigned long  beforeLastPrinted = 0;
+
+	for (i = 1; i <= 99; i++)
+	{
+		int n1 = i - 1;
+		int n2 = i - 2;
+
+		if (n1 > 0 && n2 > 0)
+		{
+			unsigned long temp = lastPrinted;
+
+			lastPrinted += beforeLastPrinted;
+			printf("%ld", lastPrinted);
+			beforeLastPrinted = temp;
+		}
+		else
+		{
+			printf("%d", i);
+			beforeLastPrinted = lastPrinted;
+			lastPrinted = i;
+		}
+
+		if (i < 50)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+	return (0);
+}
