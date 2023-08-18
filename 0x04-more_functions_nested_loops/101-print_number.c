1e4be831046f0660a4ad9643a07139de71d636d0
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-18 16:45:15 -0700

    print numbers in digit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_number  - main function
+ * @n: number tp print
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar(n % 10 + '0');
+}
