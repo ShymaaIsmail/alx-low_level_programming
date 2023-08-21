ab148cef671f30e439560199c1cee01855f6e5a0
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-21 15:28:15 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *print_rev - prints string reversly
+ *@s: pointer to s
+ *Return: void
+ */
+
+void print_rev(char *s)
+{
+	strrev(s);
+}
