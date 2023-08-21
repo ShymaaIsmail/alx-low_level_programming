4c9443ef67d717e73d94fc8216c2d5f157fbd701
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-21 15:54:41 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *rev_string - prints string reversly
+ *@s: pointer to s
+ *Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int length = strlen(s);
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
+}
