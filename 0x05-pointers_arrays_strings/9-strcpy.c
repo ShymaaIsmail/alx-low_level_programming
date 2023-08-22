61f6c35ae331e09704d31e22354626015b8c39f1
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-21 18:54:07 -0700

    stcopy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *_strcpy - string copy and return pointer
+ *@dest: pointer to the destination
+ *@src: pointer to the source
+ *Return: return a point of string to the destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int length = strlen(src);
+
+	for (i = 0; i < length; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (*dest);
+}
