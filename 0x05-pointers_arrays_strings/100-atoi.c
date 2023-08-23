3fcfffc99d59220e18a8af36864e3966bbc6ac9b
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-23 16:43:05 -0700

    fix atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "main.h"
 
 /**
@@ -11,7 +12,32 @@
 
 int _atoi(char *s)
 {
-	int i;
-	i = atoi(s);
-	return (i);
+	int n = 0;
+	int signToPrint = 1;
+
+	while ((*s != '\0' && (*s < '0' || *s > '9')))
+	{
+		switch (*s)
+		{
+		case ' ':
+		s++;
+		break;
+		case '+':
+		{
+			s++;
+			break;
+		}
+		case '-':
+		{
+			signToPrint = signToPrint * -1;
+			s++;
+			break;
+		}
+		default:
+		s++;
+		break;
+		}
+	}
+	n = atoi(s);
+	return (signToPrint * n);
 }
