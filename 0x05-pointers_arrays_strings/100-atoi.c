942e39fdf57a7ae908c1502bf03036d451f2728d
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-23 14:51:11 -0700

    first atoi version

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *_atoi - string copy and return pointer
+ *@s: pointer to a string
+ *Return: return integer
+ */
+
+int _atoi(char *s)
+{
+	int i;
+	i = atoi(s);
+	return (i);
+}
