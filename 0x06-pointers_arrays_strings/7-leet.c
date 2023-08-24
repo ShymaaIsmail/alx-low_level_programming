68ac08e9a589251f22a1495bd0b588e39216b32d
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-24 16:37:57 -0700

    fix let

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,7 +12,7 @@
 char *leet(char *s)
 {
 	char letters[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	char codes[] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
+	char codes[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 	int i, l;
 
 	for (i = 0; s[i] != '\0' ; i++)
@@ -21,7 +21,7 @@ char *leet(char *s)
 		{
 			if (s[i] == letters[l])
 			{
-				s[i] = codes[l] + '0';
+				s[i] = codes[l];
 			}
 		}
 	}
