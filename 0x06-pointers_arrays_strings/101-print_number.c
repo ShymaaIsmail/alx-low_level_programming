37c039b190e21833bcafed52c37f222ae326ce82
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-25 15:30:36 -0700

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_number  - main function
+ * @n: number tp print
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	unsigned int un = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		un = un * -1;
+	}
+
+	if (un / 10)
+	{
+		print_number(un / 10);
+	}
+
+	_putchar(un % 10 + '0');
+}}
