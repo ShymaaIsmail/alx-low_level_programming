1c54d04d547268906883fc5a81931899824bd749
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-30 23:57:56 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,6 +3,29 @@
 #include "main.h"
 
 /**
+ *iterator -  checks the number sqrt
+ *@i: index
+ *@number: the number
+ *Return: natual sqrt.
+ */
+int iterator(int i, int number)
+{
+	int is_prime = 0;
+
+	if (number <= 1 || n % i == 0)
+	{
+		is_prime = 0;
+	}
+	if (i == 1)
+	{
+		is_prime = 1;
+	}
+	i++;
+	is_prime = iterator(i, number);
+	return (is_prime);
+}
+
+/**
  *is_prime_number  - returns whether n is prime number or not 
  *@n: number
  *Return: returns 1 if prime number , 0 if not.
@@ -10,9 +33,7 @@
 
 int is_prime_number(int n)
 {
-	int isPrime= 0;
-
+	int isPrime = iterator(1, n);
 
 	return (isPrime);
 }
-
