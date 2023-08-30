08b3dcd1631f946e5f0688573787e69cb15af90d
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-08-30 00:07:10 -0700

    puts with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *_puts_recursion - prints a string
+ *@s: pointer to  string
+ *Return: nothing void
+ */
+
+void _puts_recursion(char *s)
+{
+	char *remaning_text = s + 1;
+
+	if (strlen(s) > 0) 
+	{
+        	_putchar(*s);
+		_puts_recursion(remaning_text);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
