b76765f48038c6583d41c75787501c418f87742f
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-01 04:19:33 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
 
+
+/**
+ *iterator - returns 1 if a string is a palindrome and 0 if not.
+ *@s1: first string to check
+ *@s2: second string 
+ *Return: i or zero.
+ */
+int iterator(char *s1, char *s2)
+{
+	if (i >= j)
+		return (1);
+
+	if (s[i] != s[j])
+		return (0);
+
+	return (iterator(s1, s2));
+}
+
+/**
+ *wildcmp - returns 1 if both strings are identical,  otherwise 0
+ *@s1: string 1
+ *@s2: string 2
+ *Return: i or zero.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int is_identical = 0;
+
+	is_identical = iterator(s1, s2);
+	return (is_identical);
+}
