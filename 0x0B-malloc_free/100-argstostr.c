6f07ead82640f4327b6f1daa0fe5ae0020aaed59
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-05 01:41:07 -0700

    fix counters

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,13 +11,35 @@
 char *argstostr(int ac, char **av)
 {
 	char *full_args_str;
-	int a;
+	int arg_index, arg_char_index, part = 0, total_chars_length = 0;
 
-	for(a = 0; a < ac; a++)
+	if (ac == 0 || av == NULL)
 	{
-		full_args_str = av[a];
-		full_args_str++;
+		full_args_str = NULL;
 	}
-	full_args_str = '\0';
+	else
+	{
+	for (arg_index = 0; arg_index < ac; arg_index++)
+	{
+		int arg_length = strlen(av[arg_index]);
+
+		total_chars_length += arg_length;
+	}
+	full_args_str = (char *)malloc(sizeof(char) * (total_chars_length + 1));
+	if (full_args_str != NULL)
+	{
+		for (arg_index = 0; arg_index < ac; arg_index++)
+		{
+			for (arg_char_index = 0;
+					av[arg_index][arg_char_index] != '\0';
+					arg_char_index++)
+			{
+				full_args_str[part++] = av[arg_index][arg_char_index];
+			}
+			full_args_str[part++] = '\n';
+		}
+		full_args_str[part] = '\0';
+	}
+	}
 	return (full_args_str);
 }
