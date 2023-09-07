23af32a38eaac6fb4bf0eb24d930cb988df6dbde
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-06 23:00:17 -0700

    strow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 /**
  *strtow  - concatenates two strings
- *@s1: str
+ *@str: str
  *Return: pointer to the concatenated string
  */
 char **strtow(char *str)
 {
 	char **array;
-	int i, j, words_count, char_length, str_length;
+	int c, w, words_count = 0, char_length = 0, str_length = 0;
 
-	if (str == NULL || *str == "")
+	if (str == NULL || str[0] == '\0')
 	{
 		array = NULL;
 	}
 	else
 	{
 		str_length = strlen(str);
-		for (i = 0; i < str_length; i++);
+		for (c = 0; c < str_length; c++)
 		{
-			if (str[i] == " ")
+			if (isspace(str[c]))
 			{
 				words_count++;
 			}
@@ -30,10 +31,30 @@ char **strtow(char *str)
 				char_length++;
 			}
 		}
-		array = (char *)malloc(sizeof(char) * words_count);
-		for (i = 0, i < words_count; i++)
+		array = (char **)malloc(sizeof(char *) * (words_count + 1));
+		if (array != NULL)
 		{
-			arra
+		for (w = 0; w < words_count; w++)
+		{
+			int w_c_index = 0;
+
+			array[w] = (char *)malloc(sizeof(char) * (str_length + 1));
+			if (array[w] != NULL)
+			{
+			for (c = 0; c < str_length; c++)
+			{
+				if (!isspace(str[c]))
+				{
+					array[w][w_c_index] = str[c];
+					w_c_index++;
+				}
+				else
+					break;
+			}
+			array[w][w_c_index] = '\0';
+			}
+		}
+		array[words_count] = NULL;
 		}
 	}
 	return (array);
