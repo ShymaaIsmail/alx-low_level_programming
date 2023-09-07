4158f81e2a9f43ae5496a9633ca4034144480a81
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-08 02:36:20 +0300

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,17 +10,16 @@
  */
 int *array_range(int min, int max)
 {
-  if (min > max)
-  {
-    return (NULL);
-  }
-  else
-  {
-    int *array = calloc(((max - min) + 1), sizeof(int));
+if (min > max)
+{
+  return (NULL);
+}
+else
+{
+int *array = calloc(((max - min) + 1), sizeof(int));
 
-    if (array != NULL)
+if (array != NULL)
     {
-      
     }
     return (array);
   }
