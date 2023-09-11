60753257d1dc3e05a2ae26e1827ac9c35fb0fbd4
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-11 13:54:39 +0300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
-*main - print file name using  macro 
+*main - print file name using  macro
 *Return: int
 */
 
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	return(0);
+	return (0);
 }
