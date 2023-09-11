5702f9f32b0199da3c2db907eb6ec1cc5c24799a
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-12 00:45:46 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,9 +13,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 dog_t *baby_dog = malloc(sizeof(dog_t));
 if (baby_dog != NULL)
 {
-baby_dog.name = name;
-baby_dog.age = age;
-bretudog.owner = owner;
+baby_dog->name = name;
+baby_dog->age = age;
+baby_dog->owner = owner;
 }
 return (baby_dog);
 }
