ede9be32d8a9917cf666e81b297f6b9cbd6fb61e
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-11 23:21:52 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,1 +1,38 @@
 
+#include <stdio.h>
+#include "dog.h"
+
+/**
+*print_dog - printing details of a dog pointer
+*@d: dog pointer
+*/
+void print_dog(struct dog *d)
+{
+  if (d != NULL)
+  {
+    if (d->name != NULL)
+    {
+      printf("Name: %s\n", d->name);
+    }
+    else
+    {
+      printf("Name: (nil)\n");
+    }
+    if (d->age != NULL)
+    {
+      printf("Age: %.6f\n", d->age);
+    }
+    else
+    {
+      printf("Age: (nil)\n");
+    }
+    if (d->owner != NULL)
+    {
+      printf("Owner: %s\n", d->owner);
+    }
+    else
+    {
+      printf("Owner: (nil)\n");
+    }
+  }
+}
