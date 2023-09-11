6d0405cb68eb2e956cd6e851c4fcf87530d5dbe8
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-12 00:37:21 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,1 +1,20 @@
+include <stdio.h>
+#include "dog.h"
 
+/**
+*new_dog - creates a new instance of the dog
+*@name: name
+*@age: age
+*owner: owner
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *baby_dog = malloc(sizeof(dog_t));
+if (baby_dog != NULL)
+{
+baby_dog.name = name;
+baby_dog.age = age;
+bretudog.owner = owner;
+}
+return (baby_dog);
+}
