d3842094c167c3e3e380e50e4e8d8b874f0c99dd
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-11 22:40:19 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef _dog_
 #define _dog_
 
+/**
+ * struct dog - task zero requirement
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: dog struct for task zero
+ */
 struct dog
 {
 char *name;
