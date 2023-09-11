42535335c0d2bbc48d71fffb93e50dd6c33e8ac2
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-11 23:57:07 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,11 @@ float age;
 char *owner;
 };
 
+/**
+*dog_t: dog type
+*/
+typedef dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
