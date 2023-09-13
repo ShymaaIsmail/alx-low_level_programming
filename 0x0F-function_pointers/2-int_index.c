25eabaa85d7121de5710ca3f71e74fe2d73ed0af
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-13 15:11:44 +0300

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+/**
+ *int_index - searches for integer in array
+ *@array: pointer to an array of integer data type
+ *@size: array number of elements
+ *@cmp: pointer to a function that search for the integer
+ *Return: returns the index of the first element for which
+ *the cmp function does not return 0
+ * return -1 if no elements found or sizeb<= 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int index_to_return = -1;
+if (size > 0)
+{
+}
+return (index_to_return);
+}
