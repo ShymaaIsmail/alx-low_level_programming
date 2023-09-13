af4cd5285f4622359cf813cf53eafbc0a277366b
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-13 02:22:56 -0700

    task array iterator in progress

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *array_iterator - iterrate over array
+ *@array: pointer to array of int
+ *@size: array size
+ *@action: pointer to function that takes int and doesn't return
+ *Return: no return
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+}
