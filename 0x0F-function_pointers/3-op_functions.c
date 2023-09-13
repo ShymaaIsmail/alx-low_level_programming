7d183b26ac5d0a9bcbd71cf102352251aef63d44
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-13 15:43:41 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -9,19 +9,36 @@
 int op_add(int a, int b)
 {
 	int result;
+
+	result = a + b;
 	return (result);
 }
 
-
+/**
+ * op_sub - subtract 2 numbers
+ * @a: first integr operand
+ * @b: second integer operand
+ * Return: returns the subtraction of the 2 integer variables
+ */
 int op_sub(int a, int b)
 {
         int result;
+
+	result = a - b;
         return (result);
 }
 
+/**
+ * op_mul - multiply 2 numbers
+ * @a: first integr operand
+ * @b: second integer operand
+ * Return: returns the multiplication of the 2 integer variables
+ */
 int op_mul(int a, int b)
 {
         int result;
+
+	result = a * b;
         return (result);
 }
 
