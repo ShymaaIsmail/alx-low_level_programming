ded6d0fed5a9815c00d7a8f8c9e56b33a66759b5
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-13 19:48:24 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,7 +3,8 @@
 /**
  * get_op_fun - select the appropriate function based on th s operator
  * @s: operator
- * Return: returns pointer to the function that corresponds to the operator given as a parameter.
+ * Return: returns pointer to the function
+ * that corresponds to the operator given as a parameter.
  * 
  */
 int (*get_op_func(char *s))(int, int)
