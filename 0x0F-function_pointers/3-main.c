5c48104c08d9c2ac3a6de0cfd19ca0255588f2fb
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-14 05:04:43 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,7 +19,6 @@ if (argc != 4)
 {
 printf("Error\n");
 exit(98);
-return (98);
 }
 
 num1 = atoi(argv[1]);
@@ -29,14 +28,12 @@ if ((*operator == '%' || *operator == '/') && num2 == 0)
 {
 printf("Error\n");
 exit(100);
-return (100);
 }
 operation_func = get_op_func(operator);
 if (operation_func == NULL)
 {
 printf("Error\n");
 exit(99);
-return (99);
 }
 result = operation_func(num1, num2);
 printf("%d\n", result);
