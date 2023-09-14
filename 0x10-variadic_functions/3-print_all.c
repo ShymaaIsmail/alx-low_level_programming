864c57c7490f75d03c1a22e694f099b309ab7825
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-14 20:42:50 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -45,14 +45,7 @@ void print_string(va_list argument)
 {
 char *string_to_print = va_arg(argument, char*);
 
-if (*string_to_print == '\0')
-{
-printf("(nil)");
-}
-else
-{
 printf("%s", string_to_print);
-}
 }
 
 /**
