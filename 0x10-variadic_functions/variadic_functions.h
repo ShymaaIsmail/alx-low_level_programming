530ecd86db7f9aba08ffdf9436dc5c317cb76b60
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-14 16:11:27 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,10 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+typedef struct spec
+{
+char *symbol;
+int (*print)();
+} spec_type;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
