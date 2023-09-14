b9867ffc9411badcaedc8e0dee751fa71ca0829d
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-14 04:25:40 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,27 +9,27 @@
  * Return: Always 0.
  */
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        printf("Error\n");
-        exit(98);
-        return (98);
-    }
+if (argc != 4) {
+printf("Error\n");
+exit(98);
+return (98);
+}
 
-    int num1 = atoi(argv[1]);
-    char *operator = argv[2];
-    int num2 = atoi(argv[3]);
+int num1 = atoi(argv[1]);
+char *operator = argv[2];
+int num2 = atoi(argv[3]);
 if ((operator == '%' || operator == '/') && num2 == 0)
 {
 printf("Error\n");
-    exit(100);
-    return (100);
+exit(100);
+return (100);
 }
-    int (*operation_func)(int, int) = get_op_func(operator);
+int (*operation_func)(int, int) = get_op_func(operator);
 
-    if (operation_func == NULL) {
-        printf("Error\n");
-        exit(99);
-        return (99);
+if (operation_func == NULL) {
+printf("Error\n");
+exit(99);
+return (99);
 }
 int result = operation_func(num1, num2);
 
