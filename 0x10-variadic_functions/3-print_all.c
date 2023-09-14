33f23a15d159ca79aa59d162a4eeb1cbd8f7a834
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-14 17:13:20 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,40 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
- *print_strings -  prints arguments based on format specifiers
- *@format: separator between printed numbers
+*print_char - print one char
+*@argument: pointer to the last variadic argument
+*/
+void print_char(va_list argument)
+{
+char char_to_print = va_arg(argument, char);
+
+printf('%c', char_to_print);
+}
+
+/**
+*print_integer - print one iteger
+*@argument: pointer to the last variadic argument
+*/
+void print_integer(va_list argument)
+{
+int int_to_print = va_arg(argument, int);
+
+printf('%i', int_to_print);
+}
+
+/**
+*print_float - print one float
+*@argument: pointer to the last variadic argument
+*/
+void print_float(va_list argument)
+{
+float float_to_print = va_arg(argument, float);
+
+printf('%f', float_to_print);
+}
+
+
+/**
+*print_string - print one string
+*@argument: pointer to the last variadic argument
+*/
+void print_string(va_list argument)
+{
+char *string_to_print = va_arg(argument, char*);
+
+printf('%s', string_to_print);
+}
+
+/**
+ *print_all -  prints arguments based on format specifiers
+ *@format: characters as specifiers 
  *@...: undefinite number of  arguments with different data types
  *Return: nothing
  */
 void print_all(const char * const format, ...)
 {
-		unsigned int param_index = 0;
-		va_list last_fixed_arg_ptr;
+va_list arguments_pointer;
+int format_index = 0;
+int specifier_index = 0;
+spec_type specifiers[] = {
+{"c", print_char},
+{"i", print_integer},
+{"f", print_float},
+{"s", print_string}
+};
 
-		if (n > 0)
-		{
-		va_start(last_fixed_arg_ptr, n);
-		for (param_index = 0; param_index < n; param_index++)
-		{
-			char *str_to_print = va_arg(last_fixed_arg_ptr, char *);
-
-			if (str_to_print != NULL)
-			{
-				printf("%s", str_to_print);
-			}
-			else
-			{
-				printf("(nil)");
-			}
-
-			if (separator != NULL && param_index != n - 1)
-			{
-				printf("%s", separator);
-			}
-		}
-		va_end(last_fixed_arg_ptr);
-		printf("\n");
-		}
+va_start(arguments_pointer, format);
+while (format != '\0')
+{
+while_specifier:
+while (specifier_index < 4)
+{
+if(format[format_index] == specifiers[specifier_index].symbol)
+{
+specifiers[specifier_index].print(arguments_pointer);
+break while_specifier;
+}
+}
+}
+printf("\n");		
 }
