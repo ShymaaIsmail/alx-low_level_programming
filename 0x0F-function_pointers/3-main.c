62b166eddf8e9f811fe7e6b74cbdd21008073953
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-14 04:38:05 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,6 +11,8 @@
  */
 int main(int argc, char *argv[])
 {
+int num1, num2, operator;
+
 if (argc != 4)
 {
 printf("Error\n");
@@ -18,9 +20,9 @@ exit(98);
 return (98);
 }
 
-int num1 = atoi(argv[1]);
-char *operator = argv[2];
-int num2 = atoi(argv[3]);
+num1 = atoi(argv[1]);
+operator = argv[2];
+num2 = atoi(argv[3]);
 if ((operator == '%' || operator == '/') && num2 == 0)
 {
 printf("Error\n");
