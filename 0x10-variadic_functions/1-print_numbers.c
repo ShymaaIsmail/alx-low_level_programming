3aafa335e1d50b8232bbc720d7e2265560f14b95
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-14 14:11:03 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -29,7 +29,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 				printf("%s", separator);
 			}
 		}
-		va_end(last_fixed_arg_ptr);
 		printf("\n");
+		va_end(last_fixed_arg_ptr);
+
 		}
 }
