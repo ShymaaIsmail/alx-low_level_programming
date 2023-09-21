bcd5aab05778ae29ad8cd49d9420240790715958
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-21 21:59:09 +0300

    Create 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+*free_list - f
+*@head: head
+*Return: no return
+*/
+void free_list(list_t* head)
+{
+list_t* current = head;
+list_t* next;
+
+while (current != NULL)
+{
+next = current->next;
+free(current);
+current = next;
+}
+}
