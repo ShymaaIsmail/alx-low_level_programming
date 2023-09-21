cde48fa516fd9db8784cdeeacc9c1a0ec06ee224
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-21 21:21:49 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,19 +12,22 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    if (str == NULL) {
-        return NULL;
-    }
-    list_t *new_node = (list_t *)malloc(sizeof(list_t));
-    if (new_node == NULL) {
-        return NULL;
-    }
-    new_node->str = strdup(str);
-    if (new_node->str == NULL) {
-        free(new_node);
-        return NULL;
-    }
-    new_node->next = *head;
-    *head = new_node;
-    return (new_node);
+if (str == NULL)
+{
+return (NULL);
+}
+list_t *new_node = (list_t *)malloc(sizeof(list_t));
+
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->str = strdup(str);
+if (new_node->str == NULL) {
+free(new_node);
+return (NULL);
+}
+new_node->next = *head;
+*head = new_node;
+return (new_node);
 }
