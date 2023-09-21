a789a3a3e48f205df6cce1a4deb8be66bdb6cb9e
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-21 19:51:41 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,1 +1,16 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ *print_list -  prints list of elements
+ *@h: pointer to  list of nodes of type list_t
+ *Return: size of list elements
+ */
+size_t print_list(const list_t *h)
+{
+int size = 0;
+
+return size;
+}
