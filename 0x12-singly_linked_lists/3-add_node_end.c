3c91e39ff40021639ac033662d3f38bc0f9ee299
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-21 21:52:59 +0300

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include "lists.h"
 
+/**
+*add_node_end - add_node_end
+*@head: head
+*@str: str
+*Return: ret
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *new_node = (list_t *)malloc(sizeof(list_t));
