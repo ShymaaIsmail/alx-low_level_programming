415dcc73651f8007e0025257b4e63bf3e80a4ce9
shymaaismail <shymaa.m.ismail@gmail.com>
2023-09-26 08:53:17 +0300

    refactor task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,15 +11,12 @@
 size_t print_listint(const listint_t *h)
 {
 	int nodes_count = 0;
-	listint_t *temp_node = NULL;
 
-	if (h != NULL)
-	{
-		do {
-			printf("%d\n", temp_node == NULL ? h->n : temp_node->n);
-			temp_node = temp_node == NULL ? h->next : temp_node->next;
+		while (h != NULL)
+		{
+			printf("%d\n", h == NULL ? 0 : h->n);
+			h = h->next;
 			nodes_count++;
-		} while (temp_node != NULL);
-	}
+		}
 	return (nodes_count);
 }
