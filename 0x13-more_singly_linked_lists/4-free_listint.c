bddd58eb7476b6b1ffa3f19366255eae16a633dd
shymaaismail <shymaa.m.ismail@gmail.com>
2023-09-26 11:29:06 +0300

    free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,4 +10,15 @@
  */
 void free_listint(listint_t *head)
 {
+	listint_t *temp;
+
+	if (head != NULL)
+	{
+		while (head != NULL)
+		{
+			temp = head;
+			head = head->next;
+			free(temp);
+		}
+	}
 }
