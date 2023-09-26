9ac7f40cda992123b2e77f28ad4f03658cd3efe5
shymaaismail <shymaa.m.ismail@gmail.com>
2023-09-26 12:13:12 +0300

    find node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,11 +4,32 @@
 #include "lists.h"
 
 /**
- *get_nodeint_at_index - find and return the node at certain index
- *@head: pointer to  head
- *@index: required index
- *Return: pointer to the found node
- */
+*get_nodeint_at_index - find and return the node at certain index
+*@head: pointer to  head
+*@index: required index
+*Return: pointer to the found node
+*/
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	unsigned int iterator = 0;
+
+	listint_t *found_node = NULL;
+
+	if (head != NULL)
+	{
+		while (head != NULL)
+		{
+			if (iterator == index)
+			{
+				found_node = head;
+				break;
+			}
+			else
+			{
+				head = head->next;
+			}
+			iterator++;
+		}
+	}
+	return (found_node);
 }
