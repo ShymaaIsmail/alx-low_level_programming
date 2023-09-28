30bf140b516642262fb4093c19e9a154514645ff
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-29 02:42:02 +0300

    Create 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+*extract_binary_by_index - f
+*@n: n
+*@index: to find
+*Return: void
+*/
+int extract_binary_by_index(unsigned long int *n, unsigned long int index)
+{
+unsigned long int mask = 1UL << index;
+int b_value = -1;
+
+if (index <= 64)
+{
+*n ^= mask;
+b_value = 1;
+}
+
+return (b_value);
+}
+
+/**
+*clear_bit - f
+*@n: n
+*@index: in
+*Return: int
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+int bit_found = -1;
+
+bit_found = extract_binary_by_index(n, index);
+
+
+return (bit_found);
+}
