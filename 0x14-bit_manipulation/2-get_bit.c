1681ee48cde9faa083fefdc67663a49d53043208
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-28 22:18:53 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,8 @@ int extract_binary_by_index(unsigned long int n, unsigned long int index)
 {
 unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
 int leadingZeros = 1;
-int position = 0, b_value = -1;
+unsigned long int position = 0;
+int b_value = -1;
 if (n > 0)
 {
 while (mask > 0)
