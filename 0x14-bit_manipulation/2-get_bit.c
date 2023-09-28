7432242df74713d361a7375200985f1bafd41a47
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-28 22:02:30 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,33 +1,41 @@
 #include "main.h"
 
 /**
-*print_binary - f
+*extract_binary_by_index - f
 *@n: n
+*@index: to find
 *Return: void
 */
-void print_binary(unsigned long int n)
+int extract_binary_by_index(unsigned long int n, unsigned int index)
 {
 unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
 int leadingZeros = 1;
-
-if (n == 0)
+int position = 0, b_value = -1;
+if (n > 0)
 {
-_putchar('0');
-return;
-}
 while (mask > 0)
 {
 if ((n & mask) != 0)
 {
 leadingZeros = 0;
-_putchar('1');
+b_value = 1;
 }
 else if (!leadingZeros)
 {
-_putchar('0');
+b_value = 0;
 }
 mask >>= 1;
 }
+if (index == position)
+{
+break;
+}
+else
+{
+  position++;
+}
+}
+return (b_value);
 }
 
 /**
@@ -39,6 +47,8 @@ mask >>= 1;
 int get_bit(unsigned long int n, unsigned int index)
 {
 int bit_found = -1;
+
+bit_found = extract_binary_by_index(n, index);
 
 
 return (bit_found);
