d362e5ad0d8afd1f173bb7284c69ce73c79de99f
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-28 17:44:55 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,1 +1,25 @@
+#include "main.h"
 
+void print_binary(unsigned long int n)
+{
+if (n == 0)
+{
+_putchar('0');
+return;
+}
+unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+int leadingZeros = 1;
+while (mask > 0)
+{
+if ((n & mask) != 0)
+{
+leadingZeros = 0;
+_putchar('1');
+}
+else if (!leadingZeros)
+{
+_putchar('0');
+}
+mask >>= 1;
+}
+}
