a9f688049e7b489e5d980cfe2f55aeb1e4cf8343
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-28 15:24:43 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,31 @@
+#include <main.h>
+/**
+*
+*
+*
+*/
 
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int convered_value = 0;
+int length = 0, index = 0;
+
+if (b != NULL)
+{
+length = strlen(b);
+for (index = length - 1 ;index >= 0; index--)
+{
+if(b[index] == 0 || b[index] == 1)
+{
+result = 
+}
+else
+{
+result = 0;
+break;
+}
+}
+}
+
+return (converted_value);
+}
