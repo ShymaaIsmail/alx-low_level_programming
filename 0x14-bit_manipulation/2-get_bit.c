edf19461d679524af2e7b06b3dcf41b13329dd3e
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-28 21:49:51 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,1 +1,45 @@
+#include "main.h"
 
+/**
+*print_binary - f
+*@n: n
+*Return: void
+*/
+void print_binary(unsigned long int n)
+{
+unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+int leadingZeros = 1;
+
+if (n == 0)
+{
+_putchar('0');
+return;
+}
+while (mask > 0)
+{
+if ((n & mask) != 0)
+{
+leadingZeros = 0;
+_putchar('1');
+}
+else if (!leadingZeros)
+{
+_putchar('0');
+}
+mask >>= 1;
+}
+}
+
+/**
+*get_bit - f
+*@n: n
+*@index: in
+*Return: int
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+int bit_found = -1;
+
+
+return (bit_found);
+}
