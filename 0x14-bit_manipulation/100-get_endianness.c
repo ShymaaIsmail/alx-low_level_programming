4f97eb352def91d827683c9e5b8ea94afc95bb74
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-29 18:13:51 +0300

    Create 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*get_endianness - g
+*Return: int
+*
+*
+*/
+int get_endianness(void)
+{
+unsigned int num = 1;
+char* byte = (char*)&num;
+
+if (*byte == 1)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
