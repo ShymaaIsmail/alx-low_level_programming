c655c2ac4595fce01dcab591501031c790c1975d
shymaaismail <shymaa.m.ismail@gmail.com>
2023-10-02 11:29:34 +0300

    read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,1 +1,35 @@
+#include "main.h"
 
+/**
+* read_textfile - read_textfile
+* @filename: filename
+* @letters: letters
+* Return: ssize_t
+*
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t actual_letters = 0;
+	int fd, write_result;
+
+	char *buff;
+
+	if (filename != NULL)
+	{
+		fd = open(filename, O_RDONLY);
+		if (fd >= 0)
+		{
+		buff = (char *)malloc(letters * sizeof(char));
+		actual_letters = read(fd, buff, letters);
+		buff[fd] = '\0';
+		write_result = write(STDOUT_FILENO, buff, actual_letters);
+		if (write_result == -1)
+		{
+			actual_letters = 0;
+		}
+		close(fd);
+		free(buff);
+		}
+	}
+	return (actual_letters);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,1 +1,19 @@
+#ifndef _file_io_
+#define _file_io_
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <math.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+#endif
 
