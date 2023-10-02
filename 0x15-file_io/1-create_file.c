09bb569fa5bdd21159b713497dc43694b8846202
shymaaismail <shymaa.m.ismail@gmail.com>
2023-10-02 11:32:44 +0300

    add create file prototype

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,21 @@
 
+#include "main.h"
+
+/**
+* create_file - create_file
+* @filename: filename
+* @text_content: text_content
+* Return: int
+*
+*/
+int create_file(const char *filename, char *text_content)
+{
+int result = -1;
+char *buff;
+
+if (filename != NULL)
+{
+
+}
+return (result);
+}
