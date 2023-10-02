a8f76ebfeb0a940a1219c20cd8b8280b8d67844f
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-10-02 23:46:44 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,17 +14,17 @@ int result = -1, fd, write_result = 0;
 
 if (filename != NULL)
 {
-  fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-  if (fd >= 0)
-  {
-    text_content = text_content == NULL ? "" : text_content;
-    write_result = write(fd, text_content, strlen(text_content));
-    if (write_result >= 0)
-    {
-      result = 1;
-    }
-    close(fd);
-  }
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd >= 0)
+{
+text_content = text_content == NULL ? "" : text_content;
+write_result = write(fd, text_content, strlen(text_content));
+if (write_result >= 0)
+{
+result = 1;
+}
+close(fd);
+}
 }
 return (result);
 }
