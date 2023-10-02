15a9609d48399622571f127434e9d8350e32f530
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-10-02 17:21:55 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,11 +10,18 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-int result = -1;
+int result = -1, fd;
 char *buff;
 
 if (filename != NULL)
 {
+  fd = open(filename, O_WRONLY);
+  if (fd >= 0)
+  {
+    dprintf(fd, "%s", text_content);
+    close(fd);
+    result = 1;
+  }
 
 }
 return (result);
