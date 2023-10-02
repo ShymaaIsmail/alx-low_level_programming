a334e5c0edba3a90e54cebbad5580cd1b98e51e9
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-10-03 00:14:00 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,1 +1,18 @@
+#include "main.h"
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+int result = 0;
+
+if (ac != 3)
+{
+dprintf(1, "Usage: cp file_from file_to");
+exit(97);
+}
+return (result);
+}
