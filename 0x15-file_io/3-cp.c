57998933c8b095de7a68f8a48df20c6bfa600bae
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-10-03 01:06:07 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,7 +29,8 @@ if (fd_source == -1)
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source_file);
 exit(98);
 }
-fd_dest = open(dest_file, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+fd_dest = open(dest_file, O_WRONLY | O_CREAT |
+O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 if (fd_dest == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest_file);
