be9163d6aa7be12f7486fd0a54098b7fb2cb8df8
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-11 00:49:07 +0300

    print 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,19 @@
 #include "lists.h"
 
 /**
- * print_dlistint - print_dlistint
- * @h: head node
- * Return: number of nodes
+* print_dlistint - print_dlistint
+* @h: head node
+* Return: number of nodes
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-    
+    size_t node_count = 0;
+
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        node_count++;
+    }
+    return (node_count);
 }
