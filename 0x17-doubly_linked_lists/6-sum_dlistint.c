f2a58d285acbd1cdea3de46cffc66aceeb640d2c
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-11 01:37:14 +0300

    6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* sum_dlistint - sum_dlistint
+* @head:head
+* Return: int
+*/
+int sum_dlistint(dlistint_t *head)
+{
+int sum = 0;
+
+dlistint_t *current_node = head;
+
+while (current_node != NULL)
+{
+	sum += current_node->n;
+	current_node = current_node->next;
+}
+
+return (sum);
+}
