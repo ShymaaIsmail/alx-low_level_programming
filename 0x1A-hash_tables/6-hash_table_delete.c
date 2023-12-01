54b752c9419aa9e5449b9c33cf61a325b0e8f631
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-12-02 02:31:13 +0300

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,3 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete all nodes of ht
+ * @ht: The hash table to free
+ * Return: void
+ */
 void hash_table_delete(hash_table_t *ht)
 {
+unsigned long int i;
+hash_node_t *current, *temp;
+int flag = 0;
+
+if (ht != NULL)
+{
+for (i = 0; i < ht->size; i++)
+{
+current = ht->array[i];
+while (current != NULL)
+{
+temp = current;
+free(temp->key);
+free(temp->value);
+current = current->next;
+free(temp);
+}
+}
+free(ht);
+}
 }
