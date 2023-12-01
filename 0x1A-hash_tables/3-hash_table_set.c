9353d91e7ba554fbef3fee1c9121fae820d05fed
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-02 00:19:13 +0300

    task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_set - hash_table_set
+* @ht: ht
+* @key: key
+* @value: value
+* Return: flag 1 or 0
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node, *temp;
+	int is_set = 0;
+	int index = 0;
+
+	if (ht != NULL && strlen(key) > 0)
+	{
+		index = key_index((const unsigned char *)key, ht->size);
+		new_node = malloc(sizeof(hash_node_t *));
+		if (new_node != NULL)
+		{
+			new_node->key = strdup((char *)key);
+			new_node->value = strdup((char *)value);
+			new_node->next = NULL;
+		}
+		if (ht->array[index])
+		{
+			temp = ht->array[index];
+			ht->array[index] = new_node;
+			new_node->next = temp;
+		}
+		else
+		{
+			ht->array[index] = new_node;
+		}
+	}
+	return (is_set);
+}
