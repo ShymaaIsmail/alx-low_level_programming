2fe5b5f54dcab0bab0c2c05c605a2194143f94b8
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-02 01:23:12 +0300

    ht het task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *current;
 	char *v = NULL;
 
-	if (key != NULL)
+	if (strcmp(key, "") > 0 && key != NULL && ht != NULL)
 	{
 		index = key_index((const unsigned char *)key, ht->size);
 		current = ht->array[index];
