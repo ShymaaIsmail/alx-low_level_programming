5c7b4a09e6c73889a2abff004943499a44f8b63b
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-02 00:38:23 +0300

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -40,15 +40,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			if (temp == NULL)
 			{
 				new_node->next = ht->array[index];
-				ht->array[index] = new_node;
-				is_set = 1;
+				ht->array[index] = new_node, is_set = 1;
 			}
 		}
 		else
-		{
-			ht->array[index] = new_node;
-			is_set = 1;
-		}
+			ht->array[index] = new_node, is_set = 1;
 		}
 	}
 	return (is_set);
