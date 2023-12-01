7f32de7cdabddc71d9b950ea9d4e7257cadad773
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-12-02 02:05:04 +0300

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,6 +1,29 @@
 #include "hash_tables.h"
 
-
+/**
+* hash_table_print - hash_table_print
+* @ht: ht
+* Return: void
+*/
 void hash_table_print(const hash_table_t *ht)
 {
+int i = 0;
+hash_node_t current;
+
+if (ht != NULL)
+{
+printf("{");
+while (i < ht->size)
+{
+current = ht->array[index];
+while (current != NULL)
+{
+printf("'%s': '%s'",current->key,
+  current->value);
+current = current->next;
+}
+i++;
+}
+printf("}");
+}
 }
