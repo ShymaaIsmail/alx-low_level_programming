63b7448b85a53209c35f810912001a858ed1943c
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-13 20:46:35 +0300

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,8 +10,43 @@
 */
 int binary_search(int *array, size_t size, int value)
 {
-	int result = -1;
+	int index = -1;
+	size_t start = 0;
+	size_t end = size - 1;
+	size_t middle = 0;
+	size_t i = 0;
 
-
-	return (result);
+	if (array != NULL && size > 0)
+	{
+		while (start <= end)
+		{
+			printf("Searching in array:");
+			for (i = start; i < end; i++)
+			{
+				printf(" %d", array[i]);
+				if (i != size - 1)
+				{
+					printf(",");
+				}
+			}
+			printf("\n");
+			middle = (start + end) / 2;
+			if (array[middle] < value)
+			{
+				start = middle + 1;
+				continue;
+			}
+			else if (array[middle] > value)
+			{
+				end = middle - 1;
+				continue;
+			}
+			else
+			{
+				index = middle;
+				break;
+			}
+		}
+	}
+	return (index);
 }
